2bc6df5812938ff1fb74d8d75ccf83cc3fb9a4df
xxxMichael <mayckolchavez27@gmail.com>
2024-12-01 13:57:02 -0500

    Se agrega el componente tableforAdmin en la pagina Admin

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import AutorizarModal from "../components/modalAutorizar.jsx";
+import TableForAdmin from "../components/tableForAdmin.jsx";
 
 function Usuario() {
   return (
@@ -7,7 +8,7 @@ function Usuario() {
       <h1 className="mb-4 text-2xl font-bold text-black">
         Solicitudes de Cambio - Admin
       </h1>
-      <AutorizarModal />
+      <TableForAdmin />
     </div>
   );
 }
