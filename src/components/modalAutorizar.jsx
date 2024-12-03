bc6819df2e0c3089b164b21fdb9c67d325e2e71f
David Giler Rizzo <davidgiler21@gmail.com>
2024-12-03 11:00:54 -0500

    Cambio de diseño del modal de Admin

--- src/components/modalAutorizar.jsx
+++ src/components/modalAutorizar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { CheckCircleIcon, XCircleIcon } from "@heroicons/react/24/solid";
 
 const ModalComponent = ({ closeModal }) => {
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -9,79 +10,102 @@ const ModalComponent = ({ closeModal }) => {
 
     return (
         <div>
-            
-
-            {/* Modal */}
-            { (
+            {(
                 <div class="py-12 bg-gray-700 bg-opacity-50 transition-opacity duration-150 ease-in-out z-10 fixed inset-0 flex items-center justify-center">
+
                     <div class="container mx-auto w-11/12 md:w-2/3 max-w-3xl h-auto max-h-screen overflow-y-auto">
-                        <div class="relative py-8 px-10 bg-white shadow-md rounded border border-gray-400">
-                            <h1 class="text-gray-800 font-bold text-lg tracking-normal text-center mb-6">ENCABEZADO</h1>
-                            <div class="grid grid-cols-2 gap-4 mb-4">
-                                <div>
-                                    <label class="text-gray-800 text-sm font-bold leading-tight">Código</label>
-                                    <input class="mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 pl-3 text-sm border-gray-300 rounded border" />
+                        <div class="relative rounded-xl bg-gray-900 shadow-md border border-gray-400">
+                            <div className="bg-gradient-to-r from-sky-600 to-indigo-700 text-white p-6 rounded-t-xl flex justify-between items-center">
+                                <div className="flex items-baseline gap-56">
+                                    <h2 className="text-xl font-bold tracking-tight">
+                                        Confirmación de Cambios de Software
+                                    </h2>
+                                    <h2 className="text-lg p-2 font-semibold opacity-80" id="fecha">
+                                        {"21/09/2024"}
+                                    </h2>
                                 </div>
-                                <div>
-                                    <label class="text-gray-800 text-sm font-bold leading-tight">Fecha de solicitud</label>
-                                    <input type="date" class="mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 pl-3 text-sm border-gray-300 rounded border" />
+
+                                <button
+                                    onClick={closeModal}
+                                    className="hover:bg-white/20 p-2 rounded-full transition-colors"
+                                    aria-label="Cerrar modal"
+                                >
+                                    <XCircleIcon className="h-8 w-8 hover:text-red-400 transition" />
+                                </button>
+                            </div>
+                            <h2 class="text-white font-bold mt-2 text-md leading-tight mb-4">INFORMACIÓN DEL SOLICITANTE</h2>
+                            <div className="grid md:grid-cols-2 p-3 gap-6">
+                                <div class="mb-4">
+                                    <label class="block mb-2 text-sm font-semibold text-gray-700 dark:text-gray-300">Nombre del solicitante</label>
+                                    <input class="w-full px-4 py-2 border-2 rounded-lg focus:outline-none focus:ring-2" />
+                                </div>
+
+                                <div class="mb-4">
+                                    <label class="block mb-2 text-sm font-semibold text-gray-700 dark:text-gray-300">Área o dependencia</label>
+                                    <input class="w-full px-4 py-2 border-2 rounded-lg focus:outline-none focus:ring-2" />
                                 </div>
                             </div>
-                            <h2 class="text-gray-800 font-bold text-md leading-tight mb-4">Información del solicitante</h2>
-                            <div class="mb-4">
-                                <label class="text-gray-800 text-sm font-bold leading-tight">Nombre del solicitante</label>
-                                <input class="mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 pl-3 text-sm border-gray-300 rounded border" />
+                            <div class=" p-2 mb-4">
+                                <label class="block mb-2 text-sm font-semibold text-gray-700 dark:text-gray-300">Email de contacto</label>
+                                <input type="email" name="email" id='email' class="w-full px-4 py-2 border-2 rounded-lg focus:outline-none focus:ring-2" />
                             </div>
-                            <div class="mb-4">
-                                <label class="text-gray-800 text-sm font-bold leading-tight">Área o dependencia</label>
-                                <input class="mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 pl-3 text-sm border-gray-300 rounded border" />
-                            </div>
-                            <h2 class="text-gray-800 font-bold text-md leading-tight mb-4">Información del cambio</h2>
+
                             <div class="mb-4 ">
-                                <label class="text-gray-800 text-sm font-bold leading-tight">Tipo de cambio</label>
+                                <label class="block mb-2 text-sm font-semibold text-gray-700 dark:text-gray-300">Tipo de cambio</label>
                                 <div class="mt-2 flex justify-center space-x-4">
                                     <label class="inline-flex items-center">
-                                        <input type="radio" name="tipoCambio" class="form-radio h-5 w-5 text-indigo-600" />
-                                        <span class="ml-2 text-gray-800 text-sm">Estándar</span>
+                                        <input type="radio" name="tipoCambio" className="form-radio h-5 w-5 text-indigo-600" />
+                                        <span class="ml-2 dark:text-gray-300 text-sm">Estándar</span>
                                     </label>
                                     <label class="inline-flex items-center">
-                                        <input type="radio" name="tipoCambio" class="form-radio h-5 w-5 text-indigo-600" />
-                                        <span class="ml-2 text-gray-800 text-sm">Normal</span>
+                                        <input type="radio" name="tipoCambio" className="form-radio h-5 w-5 text-indigo-600" />
+                                        <span class="ml-2 dark:text-gray-300 text-sm">Normal</span>
                                     </label>
                                     <label class="inline-flex items-center">
-                                        <input type="radio" name="tipoCambio" class="form-radio h-5 w-5 text-indigo-600" />
-                                        <span class="ml-2 text-gray-800 text-sm">Emergente</span>
+                                        <input type="radio" name="tipoCambio" className="form-radio h-5 w-5 text-indigo-600" />
+                                        <span class="ml-2 dark:text-gray-300 text-sm">Emergente</span>
                                     </label>
                                 </div>
                             </div>
-                            <div class="mb-4">
-                                <label class="text-gray-800 text-sm font-bold leading-tight">Infraestructura que impacta</label>
-                                <input class="mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 pl-3 text-sm border-gray-300 rounded border" />
+                            <div class="p-2 mb-4">
+                                <label class="block mb-2 text-sm font-semibold text-gray-700 dark:text-gray-300">Descripción del cambio solicitado</label>
+                                <textarea class="w-full h-28 px-4 py-2 border-2 rounded-lg focus:outline-none focus:ring-2"></textarea>
                             </div>
-                            <div class="mb-4">
-                                <label class="text-gray-800 text-sm font-bold leading-tight">Descripción del cambio solicitado</label>
-                                <textarea class="mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-24 pl-3 text-sm border-gray-300 rounded border"></textarea>
-                            </div>
-                            <div class="mb-4">
-                                <label class="text-gray-800 text-sm font-bold leading-tight">Descripción del estado de la solicitud</label>
-                                <textarea class="mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-24 pl-3 text-sm border-gray-300 rounded border"></textarea>
-                            </div>
-                            <div class="grid grid-cols-2 gap-4 mb-6">
-                                <div>
-                                    <label class="text-gray-800 text-sm font-bold leading-tight">Validado por</label>
-                                    <input class="mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 pl-3 text-sm border-gray-300 rounded border" />
-                                </div>
-                                <div>
-                                    <label class="text-gray-800 text-sm font-bold leading-tight">Autorizado por</label>
-                                    <input class="mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 pl-3 text-sm border-gray-300 rounded border" />
+                            <h2 class="text-white font-bold mt-2 text-md leading-tight mb-4">INFORMACIÓN DE APROVACIÓN</h2>
+                            <div class="mb-4 ">
+                                <label class="block mb-2 text-sm font-semibold text-gray-700 dark:text-gray-300">Estado de cambio</label>
+                                <div class="mt-2 flex justify-center space-x-4">
+                                    <label class="inline-flex items-center">
+                                        <input type="radio" name="tipoCambio" className="form-radio h-5 w-5 text-indigo-600" />
+                                        <span class="ml-2 dark:text-gray-300 text-sm">Rechazado</span>
+                                    </label>
+                                    <label class="inline-flex items-center">
+                                        <input type="radio" name="tipoCambio" className="form-radio h-5 w-5 text-indigo-600" />
+                                        <span class="ml-2 dark:text-gray-300 text-sm">Aprovado</span>
+                                    </label>
                                 </div>
                             </div>
-                            <div class="flex items-center justify-end">
+
+                            <div class=" p-2 mb-4">
+                                <label class="block mb-2 text-sm font-semibold text-gray-700 dark:text-gray-300">Descripción del estado de la solicitud</label>
+                                <textarea class="w-full h-28 px-4 py-2 border-2 rounded-lg focus:outline-none focus:ring-2"></textarea>
+                            </div>
+                            <div class="grid p-2 grid-cols-2 gap-4 mb-6">
+                                <div>
+                                    <label class="block mb-2 text-sm font-semibold text-gray-700 dark:text-gray-300">Validado por</label>
+                                    <input class="w-full px-4 py-2 border-2 rounded-lg focus:outline-none focus:ring-2" />
+                                </div>
+                                <div>
+                                    <label class="block mb-2 text-sm font-semibold text-gray-700 dark:text-gray-300">Autorizado por</label>
+                                    <input class="w-full px-4 py-2 border-2 rounded-lg focus:outline-none focus:ring-2" />
+                                </div>
+                            </div>
+                            <div class="flex items-center mb-2 justify-center">
                                 <button class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 transition duration-150 ease-in-out hover:bg-indigo-600 bg-indigo-700 rounded text-white px-8 py-2 text-sm">
                                     Guardar
                                 </button>
-                                <button  onClick={closeModal}
-                                class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 ml-3 bg-gray-100 transition duration-150 text-gray-600 ease-in-out hover:border-gray-400 hover:bg-gray-300 border rounded px-8 py-2 text-sm">
+                                <button onClick={closeModal}
+                                    class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 ml-3 bg-gray-100 transition duration-150 text-gray-600 ease-in-out hover:border-gray-400 hover:bg-gray-300 border rounded px-8 py-2 text-sm">
                                     Cancelar
                                 </button>
                             </div>
